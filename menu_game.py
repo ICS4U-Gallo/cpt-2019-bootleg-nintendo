b4818af7eb756296df1dbe48f32eaaf070083271
cng04 <calebng.ca@gmail.com>
2020-01-07 10:55:10 -0500

    worked on this ._.

--- menu_game.py
+++ menu_game.py
@@ -1,8 +1,8 @@
 import arcade
 import settings
 
-width = settings.WIDTH
-height = settings.HEIGHT
+width = 768
+height = 640
 
 """
 bag
@@ -15,20 +15,36 @@ def main_menu():
     arcade.set_background_color(arcade.color.WHITE)
     arcade.start_render()
 
-    arcade.draw_text("Pause Menu",
+    arcade.draw_text("GAME MENU",
                      width/2, height * 0.95, arcade.color.BLACK, 50,
                      align="center", anchor_x="center", anchor_y="center")
-    arcade.draw_xywh_rectangle_outline(width/2-150,
-                                       height/2-25, 300, 75,
+
+    arcade.draw_xywh_rectangle_outline(width/4-150,
+                                       height/2-25, 325, 250,
                                        arcade.color.BLACK, 5)
-    arcade.draw_text("Play", width/2,
-                     height/2+13, arcade.color.BLACK, 30,
+    arcade.draw_text("BAG", width/4 + 12.5,
+                     height * 3/4, arcade.color.BLACK, 45,
                      align="center", anchor_x="center", anchor_y="center")
-    arcade.draw_xywh_rectangle_outline(width/2-150,
-                                       height/2-125, 300,
-                                       75, arcade.color.BLACK, 5)
-    arcade.draw_text("How to Play", width/2,
-                     height/2-87, arcade.color.BLACK, 30,
+
+    arcade.draw_xywh_rectangle_outline(width * 3/4 - 175,
+                                       height/2-25, 325, 250,
+                                       arcade.color.BLACK, 5)
+    arcade.draw_text("SAVE", width * 3/4 - 12.5,
+                     height * 3/4, arcade.color.BLACK, 45,
+                     align="center", anchor_x="center", anchor_y="center")
+
+    arcade.draw_xywh_rectangle_outline(width/4 - 150,
+                                       height/2-300, 325,
+                                       250, arcade.color.BLACK, 5)
+    arcade.draw_text("EXIT MENU", width/4 + 12.5,
+                     height/2-120, arcade.color.BLACK, 45,
+                     align="center", anchor_x="center", anchor_y="center")
+
+    arcade.draw_xywh_rectangle_outline(width * 3/4 - 175,
+                                       height/2-300, 325,
+                                       250, arcade.color.BLACK, 5)
+    arcade.draw_text("._.", width * 3/4 - 12.5,
+                     height/2-120, arcade.color.BLACK, 45,
                      align="center", anchor_x="center", anchor_y="center")
 
 
